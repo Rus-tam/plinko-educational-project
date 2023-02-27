04d1c6be4bec4a6d1319483be66c11d8f97151ea
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-02-27 09:39:40 +0500

    arbitary feature spaces

--- score.js
+++ score.js
@@ -15,7 +15,7 @@ function runAnalysis() {
     _.range(1, 15).forEach(k => {
         const accuracy = _.chain(testSet)
             .filter(testPoint => {
-                return knn(trainingSet, testPoint[0], k) === testPoint[3];
+                return knn(trainingSet, _.initial(testPoint), k) === testPoint[3];
             })
             .size()
             .divide(testSetSize)
@@ -26,8 +26,14 @@ function runAnalysis() {
 }
 
 function knn(data, point, k) {
+    // point has 3 values!!!!
     return _.chain(data)
-        .map(row => [distance(row[0], point), row[3]])
+        .map(row => {
+            return [
+                distance(_.initial(row), point),
+                _.last(row)
+            ]
+        })
         .sortBy(row => row[0])
         .slice(0, k)
         .countBy(row => row[1])
